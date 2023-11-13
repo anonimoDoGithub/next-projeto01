240d846688a86803aa460030e376a92e69a17c1c
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-13 14:35:37 -0300

    adicionando .env e simulando user

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,38 @@
 import NextAuth from "next-auth/next";
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider  from "next-auth/providers/credentials";
+import credentials from "next-auth/providers/credentials";
+
 
 const authOptions: NextAuthOptions = {
-    providers: []
+    providers: [
+        CredentialsProvider({
+            name: 'Credential',
+            credentials: {
+                email: { label: 'Email', type: 'email' },
+                password: { label: 'Password', type: 'password'}
+            },
+            async authorize(credentials){
+                const user = {
+                    id: "1",
+                    email: "user@email.com",
+                    password: "123",
+                    name: "User",
+                    role: "admin"
+                }
+
+                const isValidEmail = user.email === credentials?.email
+                const isValidPassword = user.password === credentials?.password
+
+                if (!isValidEmail || !isValidPassword) {
+                return null
+                }
+
+                return user
+            }  
+        })
+        
+    ]
 }
 
 const handler = NextAuth(authOptions)
